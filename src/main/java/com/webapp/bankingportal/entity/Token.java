d18686b2194ef0be92088a90aa34dfd22b6efc7f
Hariomlokhande-coder <158686388+Hariomlokhande-coder@users.noreply.github.com>
2026-01-31 20:03:42 +0530

    Implement token

--- src/main/java/com/webapp/bankingportal/entity/Token.java
+++ src/main/java/com/webapp/bankingportal/entity/Token.java
@@ -11,7 +11,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
